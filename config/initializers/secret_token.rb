a7a4879365a2da1b3f49cfd7648ca35f3fdb3f57
David Piegza <piegza@workhub.com>
2011-12-15 09:54:05 +0100

    Create new Rails 3.1.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPlayground::Application.config.secret_token = 'eda6eba88f44a4fd5fc82024f3e71387379240e4c83d041e25a6304ec0521d791a8f014dfa75e1c014577d66fb2bf71beacad5ef383a2d0aeb5c0dc956a6c0e2'

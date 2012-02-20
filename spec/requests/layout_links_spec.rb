2cdcf1b51a02bcbb9fbad06f6d41ab940b5b852b
David Piegza <piegza@workhub.com>
2012-02-20 10:32:16 +0100

    Use capybara in integration tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   it "should have a Home page at '/'" do
-    get '/'
-    response.should have_selector('title', content: 'Home')
+    visit '/'
+    page.should have_selector('title', text: 'Home')
   end
 
   it "should have a Contact page at '/contact'" do
-    get '/contact'
-    response.should have_selector('title', content: 'Contact')
+    visit '/contact'
+    page.should have_selector('title', text: 'Contact')
   end
 
   it "should have a About page at '/about'" do
-    get '/about'
-    response.should have_selector('title', content: 'About')
+    visit '/about'
+    page.should have_selector('title', text: 'About')
   end
 
   it "should have a Help page at '/help'" do
-    get '/help'
-    response.should have_selector('title', content: 'Help')
+    visit '/help'
+    page.should have_selector('title', text: 'Help')
   end
 end

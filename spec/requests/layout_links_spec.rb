ca902f23bae09c03eddb1758700385c537c3f97f
David Piegza <piegza@workhub.com>
2012-02-21 10:51:36 +0100

    Fix failing tests in layout links

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
   it "should have a Home page at '/'" do
-    visit '/'
+    visit root_path
     page.should have_selector('title', text: 'Home')
   end
 
   it "should have a Contact page at '/contact'" do
-    visit '/contact'
+    visit contact_path
     page.should have_selector('title', text: 'Contact')
   end
 
   it "should have a About page at '/about'" do
-    visit '/about'
+    visit about_path
     page.should have_selector('title', text: 'About')
   end
 
   it "should have a Help page at '/help'" do
-    visit '/help'
+    visit help_path
     page.should have_selector('title', text: 'Help')
   end
 end

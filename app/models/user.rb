664cf4ffa41260de7252597e86ed004bd2c7b27c
David Piegza <piegza@workhub.com>
2012-02-24 09:47:56 +0100

    Create micropost model with associations and add first tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts
+
   before_save :create_remember_token
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
